3a30b64c98b6f5586e513aa4f14b3afc4cb6fa6f
gbursson <gbursson@gmail.com>
2018-09-27 22:07:51 +0100

    Growing gulpfile.js - added several preprocessors and made sure they are working

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,54 @@
-const gulp = require('gulp');
+const {
+  gulp,
+  series,
+  parallel,
+  src,
+  dest,
+} = require('gulp');
+
 const postcss = require('gulp-postcss');
-const rename = require('gulp-rename');
 const config = require('./config.json');
 const del = require('del');
-const postcssImport = require('postcss-import');
-const simpleVars = require('postcss-simple-vars');
 const minify = require('cssnano');
-const customProps = require('postcss-custom-properties');
+const rename = require('rename');
+// const normalize = require('normalize');
+const autoprefix = require('autoprefixer');
+// const customVariables = require('postcss-css-variables');
+const customProperties = require('postcss-custom-properties');
+const mixins = require('postcss-mixins');
+// const simpleVariables = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+// const importCSS = require('gulp-cssimport');
+const concat = require('gulp-concat');
 
-// const mixins = require('postcss-mixins');
-// const nested = require('postcss-nested');
-// const customMedia = require('postcss-custom-media');
-// const metalsmith = require('gulp-metalsmith');
-// const restart = require('gulp-restart');
-// const customMedia = require('postcss-custom-media');
-// const customSelectors = require('postcss-custom-selectors');
-// const mediaMinMax = require('postcss-media-minmax');
-// const presetEnv = require('postcss-preset-env');
-// const utilities = require('postcss-utilities');
 
-const processorsCSS =[
-  postcssImport,
-  customProps,
-  simpleVars,
+// exports.default = function() {
+//   return src('src/*.js')
+//     .pipe(dest('output/'));
+// }
+
+const processorsCSS = [
+  // importCSS,
+  customProperties,
+  // simpleVariables,
+  mixins,
+  nested,
+  autoprefix,
   minify
 ]
 
 function styles() {
-  return gulp.src(config.css.src)
-    .pipe(postcss(processorsCSS))  
-    .pipe(rename("styles.min.css"))
-    .pipe(gulp.dest(config.css.out));
+  return src(config.css.src)
+    // .pipe(precss)
+    .pipe(concat('style.min.css'))
+    .pipe(postcss(processorsCSS))
+    .pipe(dest(config.css.out));
 }
 
-// gulp.task('metalsmith', function() {
-//   return gulp.src('src/**')
-//     .pipe(metalsmith())
-//     .pipe(gulp.dest('build'));
-// });
-
-function watch() {
-  gulp.watch(config.css.src, styles);
-}
-
+// const watch = () => watch(config.css.src, styles);
 const clean = () => del(config.destination);
-const build = gulp.series(clean, styles);
 
-gulp.task('watch', watch);
-gulp.task('build', build);
-gulp.task('default', build);
- 
+
+exports.styles = styles;
+exports.clean = clean;
+exports.default = series(clean, styles);
